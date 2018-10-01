0981c67a6dc131690f018bfb5ef2c0f7a176029c
grian0504 <107021035@gm.asia.edu.tw>
2018-10-01 20:55:00 +0800

    完成到ASCII碼

---
+++ src/com/company/Main.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import java.util.Scanner;
+
+        public class Main {
+
+            public static void main(String[] args) {
+
+//                /*********************
+//                 * 公升轉加侖。
+//                 **************************/
+//        /***********
+//         * 首先輸入輸入指令
+//         *************/
+//	Scanner scn = new Scanner(System.in);
+//	float l = scn.nextFloat();
+//
+//        /**********************
+//         * 接著根據題目要求
+//         * 1L = 0.26418 (gallon (US lip.))
+//         ************************/
+//    System.out.println("為\n" + (l * 0.26418f + "(gallon(US lip.))"));
+//
+//
+//                /*****************
+//                 * 稱斤論兩
+//                 **********************/
+//        Scanner scn = new Scanner(System.in);
+//        float v1 = scn.nextFloat();
+//
+//        System.out.println("為\n" + v1*600f + "克");
+//
+//                /***********************
+//                 * 計算總和、乘積、差、商和餘數
+//                 ***************************/
+//        Scanner scn = new Scanner(System.in);
+//        int v1 = scn.nextInt();
+//        int v2 = scn.nextInt();
+//
+//        if (v1>1000 || v2>1000)
+//        {
+//            System.out.println("請輸入1000以下的整數(含1000)\n");
+//        }
+//        else
+//        {
+//            System.out.println(v1 + "+" + v2 + "\n=" + (v1+v2));
+//            System.out.println(v1 + "*" + v2 + "\n=" + (v1*v2));
+//            System.out.println(v1 + "-" + v2 + "\n=" + (v1-v2));
+//            System.out.println(v1 + "/" + v2 + "\n=" + (v1/v2) + "......" + (v1%v2));
+//
+////        }
+//                /***************************
+//                 *取得ASCII碼
+//                 ********************************/
+//        Scanner scn=new Scanner(System.in);
+//        char c=scn.next().charAt(0);
+//        System.out.println(Integer.toString((int)c));
+//    }
+//}
+//
+            }
+        }
