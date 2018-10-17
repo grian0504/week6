79eb507fd8c78b37c92625494e905a254992ce7f
grian0504 <107021035@gm.asia.edu.tw>
2018-10-17 13:05:51 +0800

    完成到ASCII碼

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -50,13 +50,58 @@ import java.util.Scanner;
 //
 ////        }
 //                /***************************
-//                 *取得ASCII碼
+//                 *取得ASCII碼 (中間少了)
 //                 ********************************/
 //        Scanner scn=new Scanner(System.in);
-//        char c=scn.next().charAt(0);
+//
 //        System.out.println(Integer.toString((int)c));
 //    }
 //}
+
+                /***********************
+                 *  *取得ASCII碼-2
+                 **************************/
+//                Scanner scn = new Scanner(System.in);
+//                char ch = scn.next().charAt(0);
+//                 System.out.println((int)ch);
+
+                /***************************
+                 * 取得Unicode碼
+                 *****************************/
+//                Scanner scn = new Scanner(System.in);
+//                char ch =scn.next().charAt(0);
 //
-            }
-        }
+//                System.out.println(Integer.toHexString((int)ch));
+//
+//            }
+//        }
+//                Scanner scn = new Scanner(System.in);
+//                String str = scn.next();
+//                int v1 = Integer.parseInt(str,16);
+//                System.out.println((char)v1);
+//            }}
+                //非字母的字元不會做任何改變
+//                Scanner scn = new Scanner(System.in);
+//
+//                String str = scn.next();
+//                //轉換成大寫
+//                if (scn = str.toUpperCase());
+//                System.out.println(str.toUpperCase());
+//
+//                //轉換成小寫
+//                else if{
+//                System.out.println(str.toLowerCase());
+//            }
+//            }
+//        }
+//                String text;
+//                Scanner scn = new Scanner(System.in);
+//                text = scn.next();
+//                char= text.charAt(0);
+//                int v1 = (int);
+//                System.out.print(scn+i);
+//                if(65<=i && i<=90)
+//                {
+//                    System.out.println("")
+//                }
+//            }}
